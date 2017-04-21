390d71199130f09bf70fddeda78ae5cc1a6147ab
Keleko34 <gregory.guidero@gmail.com>
2017-04-21 14:30:34 +0200

    build and update all to prod

--- components/navbar/build/prod/navbar.js
+++ components/navbar/build/prod/navbar.js
@@ -30,7 +30,7 @@ function navbar()
 
 /* PROTOTYPES */
 
-navbar.prototype.k_html = "<!-- navbar Created by keleko34, the header navbar for the site --><div class='navbar'>  <logo></logo>  <navitem_link link='/Konnekt' left='190' title='Konnekt JS'></navitem_link>  <animated_hamburger></animated_hamburger></div>";
-navbar.prototype.k_css = "/********************************* *  navbar *  Created by keleko34 *  the header navbar for the site ********************************/.navbar {  position: relative;  height:60px;  background:#333;  border-bottom: 2px solid #000;  box-shadow: 0px 0px 8px -2px #000;}.navbar .animated_hamburger {  top:16px;  right:20px;  display:{{isMobile | isNotHidden}};}.navbar .navitem {  display:{{isMobile | isHidden}};}";
+navbar.prototype.k_html = "<!-- navbar Created by keleko34, the header navbar for the site --><div class='navbar'>  <logo></logo>  <navitem link='documentation' left='190' title='Documentation'></navitem>  <navitem link='get_started' left='313' title='Get Started'></navitem>  <navitem link='download' left='408' title='Download'></navitem>  <navitem link='playground' left='495' title='Playground'></navitem>  <animated_hamburger></animated_hamburger></div>";
+navbar.prototype.k_css = "/********************************* *  navbar *  Created by keleko34 *  the header navbar for the site ********************************/.navbar {  position: relative;  height:60px;  background:#333;  border-bottom: 2px solid #000;  box-shadow: 0px 0px 8px -2px #000;}.navbar .animated_hamburger {  top:16px;  right:20px;  display:{{isMobile | isNotHidden}};}.navbar .navitem {  display:{{isMobile | isHidden}};}.navbar .navitem_link {  display:{{isMobile | isHidden}};}";
 return navbar;
 }());
